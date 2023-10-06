6b1ba27d5b665903ccbe7af0124ac1c39baa01ed
Vimu-Kale <vimurk111@gmail.com>
2023-10-06 10:01:28 +0530

    new build

--- dist/components/Highlighter/index.js
+++ dist/components/Highlighter/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("core-js/modules/es.weak-map.js");
+require("core-js/modules/web.dom-collections.iterator.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -9,17 +11,19 @@ require("core-js/modules/es.regexp.constructor.js");
 require("core-js/modules/es.regexp.exec.js");
 require("core-js/modules/es.regexp.to-string.js");
 require("core-js/modules/es.string.replace.js");
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 var _regex = require("./regex");
 var _dompurify = _interopRequireDefault(require("dompurify"));
 var _htmlReactParser = _interopRequireDefault(require("html-react-parser"));
 var _escapeStringRegexp = _interopRequireDefault(require("escape-string-regexp"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 const Highlighter = function Highlighter() {
   let searchQuery = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
   let content = arguments.length > 1 ? arguments[1] : undefined;
   let regExp = arguments.length > 2 ? arguments[2] : undefined;
-  return () => {
+  const highlightedContent = (0, _react.useMemo)(() => {
     var _searchQuery$trim;
     if (searchQuery !== null && searchQuery !== void 0 && (_searchQuery$trim = searchQuery.trim()) !== null && _searchQuery$trim !== void 0 && _searchQuery$trim.length) {
       const escapedSearchValue = (0, _escapeStringRegexp.default)(searchQuery);
@@ -27,6 +31,7 @@ const Highlighter = function Highlighter() {
       return (0, _htmlReactParser.default)(_dompurify.default.sanitize(content === null || content === void 0 ? void 0 : content.replace(regex, '<span class="highlight">$1</span>')));
     }
     return content;
-  };
+  }, [searchQuery, content, regExp]);
+  return highlightedContent;
 };
 var _default = exports.default = Highlighter;
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,13 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true,
+  value: true
 });
 Object.defineProperty(exports, "Highlighter", {
   enumerable: true,
   get: function get() {
     return _Highlighter.default;
-  },
+  }
 });
-import "./index.css";
 var _Highlighter = _interopRequireDefault(require("./components/Highlighter"));
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
